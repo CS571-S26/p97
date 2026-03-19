d6613143f5608b909be851853ac513dcb0f7533e
Aaron Shane <aaron@Aarons-MacBook-Pro.local>
2026-03-18 19:11:24 -0500

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
+  );
+}
